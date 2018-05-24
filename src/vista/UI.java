967afc4bf896a47936a7eb4e49931a4fc2e34c87
José Emilio Gutiérrez Morán <36626945+gutierrezmoran@users.noreply.github.com>
2018-05-24 18:25:12 +0200

    + Implementación de Lambda en los botones principales

--- src/vista/UI.java
+++ src/vista/UI.java
@@ -2,6 +2,7 @@ package vista;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import vista.paneles.Articulos;
@@ -22,16 +23,16 @@ public class UI extends JFrame {
 
 	private JPanel contentPane;
 	private Articulos articulos;
-	
+
 	public UI() {
 		crearFrame();
 		crearPaneles();
 	}
-	
+
 	private void crearPaneles() {
 		this.articulos = new Articulos();
 	}
-	
+
 	private void crearFrame() {
 		setTitle("Tienda");
 		setMinimumSize(new Dimension(700, 600));
@@ -41,36 +42,51 @@ public class UI extends JFrame {
 		contentPane.setBorder(null);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JPanel panelSuperior = new JPanel();
 		panelSuperior.setBorder(new EmptyBorder(4, 4, 4, 4));
 		contentPane.add(panelSuperior, BorderLayout.NORTH);
 		panelSuperior.setLayout(new GridLayout(0, 3, 2, 0));
-		
+
+		JPanel panelPrincipal = new JPanel();
+		contentPane.add(panelPrincipal, BorderLayout.CENTER);
+		panelPrincipal.setLayout(new BoxLayout(panelPrincipal, BoxLayout.X_AXIS));
+
 		JButton botonArticulos = new JButton("Art\u00EDculos");
+		botonArticulos.addActionListener(e -> {
+			panelPrincipal.removeAll();
+			panelPrincipal.add(this.articulos, BorderLayout.CENTER);
+			contentPane.updateUI();
+		});
 		botonArticulos.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		botonArticulos.setFocusPainted(false);
 		botonArticulos.setBackground(new Color(176, 196, 222));
 		botonArticulos.setBorder(new EmptyBorder(5, 0, 5, 0));
 		panelSuperior.add(botonArticulos);
-		
+
 		JButton botonClientes = new JButton("Clientes");
+		botonClientes.addActionListener(e -> {
+			panelPrincipal.removeAll();
+			panelPrincipal.add(new JLabel("Clientes"), BorderLayout.CENTER);
+			contentPane.updateUI();
+		});
 		botonClientes.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		botonClientes.setFocusPainted(false);
 		botonClientes.setBackground(new Color(176, 196, 222));
 		botonClientes.setBorder(new EmptyBorder(5, 0, 5, 0));
 		panelSuperior.add(botonClientes);
-		
+
 		JButton botonPedidos = new JButton("Pedidos");
+		botonPedidos.addActionListener(e -> {
+			panelPrincipal.removeAll();
+			panelPrincipal.add(new JLabel("Pedidos"), BorderLayout.CENTER);
+			contentPane.updateUI();
+		});
 		botonPedidos.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		botonPedidos.setFocusPainted(false);
 		botonPedidos.setBackground(new Color(176, 196, 222));
 		botonPedidos.setBorder(new EmptyBorder(5, 0, 5, 0));
 		panelSuperior.add(botonPedidos);
-		
-		JPanel panelPrincipal = new JPanel();
-		contentPane.add(panelPrincipal, BorderLayout.CENTER);
-		panelPrincipal.setLayout(new BoxLayout(panelPrincipal, BoxLayout.X_AXIS));
 	}
 
 	public Articulos getArticulos() {
