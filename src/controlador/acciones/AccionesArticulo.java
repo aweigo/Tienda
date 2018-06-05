2ac29fd6fd390a7ea9de77e1081946784ba77633
vfern <vfern@192.168.1.109>
2018-06-05 10:51:53 +0200

    Validacion implementada

--- src/controlador/acciones/AccionesArticulo.java
+++ src/controlador/acciones/AccionesArticulo.java
@@ -4,17 +4,18 @@ import javax.swing.SwingWorker;
 
 import controlador.ParaUI;
 import modelo.data.Articulo;
+import util.validacion.Validator;
 
 public class AccionesArticulo {
 
 	private ParaUI paraUI;
-	
+
 	public AccionesArticulo(ParaUI paraUI) {
 		this.paraUI = paraUI;
 	}
-	
+
 	public void darAlta() {
-		
+
 		final SwingWorker worker = new SwingWorker() {
 
 			@Override
@@ -23,21 +24,26 @@ public class AccionesArticulo {
 				paraUI.getMensajeAltaArticulo().setText("");
 				return null;
 			}
-			
+
 		};
-		
-		int idArticulo = Integer.parseInt(this.paraUI.getIdAltaArticulo().getText());
-		String nombre = this.paraUI.getNombreAltaArticulo().getText();
-		String descripcion = this.paraUI.getDescripcionAltaArticulo().getText();
-		float precio = Float.parseFloat(this.paraUI.getPrecioAltaArticulo().getText());
-		
-		if(this.paraUI.darAltaArticulo(idArticulo, nombre, descripcion, precio)) {
-			this.paraUI.getMensajeAltaArticulo().setText("Articulo dado de alta");
-			this.paraUI.limpiarCamposAltaArticulo();
-			worker.execute();
-		} else {
-			this.paraUI.getMensajeAltaArticulo().setText("Error al dar de alta el articulo");
-			worker.execute();
+
+		if (Validator.isID(this.paraUI.getIdAltaArticulo().getText())
+				&& Validator.isNombre(this.paraUI.getNombreAltaArticulo().getText())
+				&& Validator.isPrecio(String.valueOf(this.paraUI.getPrecioAltaArticulo().getText()))) {
+
+			int idArticulo = Integer.parseInt(this.paraUI.getIdAltaArticulo().getText());
+			String nombre = this.paraUI.getNombreAltaArticulo().getText();
+			String descripcion = this.paraUI.getDescripcionAltaArticulo().getText();
+			float precio = Float.parseFloat(this.paraUI.getPrecioAltaArticulo().getText());
+
+			if (this.paraUI.darAltaArticulo(idArticulo, nombre, descripcion, precio)) {
+				this.paraUI.getMensajeAltaArticulo().setText("Articulo dado de alta");
+				this.paraUI.limpiarCamposAltaArticulo();
+				worker.execute();
+			} else {
+				this.paraUI.getMensajeAltaArticulo().setText("Error al dar de alta el articulo");
+				worker.execute();
+			}
 		}
 	}
 }
--- src/controlador/acciones/AccionesCliente.java
+++ src/controlador/acciones/AccionesCliente.java
@@ -3,6 +3,7 @@ package controlador.acciones;
 import javax.swing.SwingWorker;
 import controlador.ParaUI;
 import modelo.data.Cliente;
+import util.validacion.Validator;
 
 public class AccionesCliente {
 
@@ -13,7 +14,7 @@ public class AccionesCliente {
 	}
 
 	public void darAlta() {
-		
+
 		final SwingWorker worker = new SwingWorker() {
 
 			@Override
@@ -22,26 +23,30 @@ public class AccionesCliente {
 				paraUI.getMensajeAltaCliente().setText("");
 				return null;
 			}
-			
+
 		};
-		
+
 		String NIF = this.paraUI.getNifAltaCliente().getText();
 		String razonSocial = this.paraUI.getRazonSocialAltaCliente().getText();
 		String direccion = this.paraUI.getDireccionAltaCliente().getText();
 		String telefono = this.paraUI.getTelefonoAltaCliente().getText();
 
-		if(this.paraUI.darAltaCliente(NIF, razonSocial, direccion, telefono)) {
-			this.paraUI.getMensajeAltaCliente().setText("Cliente dado de alta correctamente");
-			this.paraUI.limpiarCamposAltaCliente();
-			worker.execute();
-		} else {
-			this.paraUI.getMensajeAltaCliente().setText("Error al dar de alta el cliente");
-			worker.execute();
+		if ((Validator.isNIF(NIF) || Validator.isCIF(NIF)) && Validator.isNombre(razonSocial)
+				&& Validator.isNombre(direccion) && Validator.isPhone(telefono)) {
+
+			if (this.paraUI.darAltaCliente(NIF, razonSocial, direccion, telefono)) {
+				this.paraUI.getMensajeAltaCliente().setText("Cliente dado de alta correctamente");
+				this.paraUI.limpiarCamposAltaCliente();
+				worker.execute();
+			} else {
+				this.paraUI.getMensajeAltaCliente().setText("Error al dar de alta el cliente");
+				worker.execute();
+			}
 		}
 	}
 
 	public void buscar() {
-		
+
 		final SwingWorker worker = new SwingWorker() {
 
 			@Override
@@ -50,22 +55,23 @@ public class AccionesCliente {
 				paraUI.getMensajeBuscarCliente().setText("");
 				return null;
 			}
-			
 		};
-		
-		Cliente cliente = this.paraUI.buscarCliente(this.paraUI.getRazonSocialBuscarCliente().getText());
 
-		if (cliente != null) {
-			this.paraUI.getCifBuscarCliente().setText(cliente.getDniCif());
-			this.paraUI.getDireccionBuscarCliente().setText(cliente.getDireccion());
-			this.paraUI.getTeléfonoBuscarCliente().setText(cliente.getTelefono());
-			this.paraUI.getBotonBorrarCliente().setEnabled(true);
-			
-			this.paraUI.getMensajeBuscarCliente().setText("Cliente encontrado correctamente");
-			worker.execute();
-		} else {
-			this.paraUI.getMensajeBuscarCliente().setText("Error al buscar el cliente");
-			worker.execute();
+		if (Validator.isNombre(this.paraUI.getRazonSocialBuscarCliente().getText())) {
+			Cliente cliente = this.paraUI.buscarCliente(this.paraUI.getRazonSocialBuscarCliente().getText());
+
+			if (cliente != null) {
+				this.paraUI.getCifBuscarCliente().setText(cliente.getDniCif());
+				this.paraUI.getDireccionBuscarCliente().setText(cliente.getDireccion());
+				this.paraUI.getTeléfonoBuscarCliente().setText(cliente.getTelefono());
+				this.paraUI.getBotonBorrarCliente().setEnabled(true);
+
+				this.paraUI.getMensajeBuscarCliente().setText("Cliente encontrado correctamente");
+				worker.execute();
+			} else {
+				this.paraUI.getMensajeBuscarCliente().setText("Error al buscar el cliente");
+				worker.execute();
+			}
 		}
 	}
 }
